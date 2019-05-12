540e10763f3e6b34e34db8b2469a52f8e023ea46
Hano <hinsul3113@gmail.com>
2019-05-11 23:27:54 -0700

    passed an object data to event object and returned like so, sls invoke local -f hello -d '{ first: 2, second: 12 }'

--- handler.js
+++ handler.js
@@ -1,15 +1,16 @@
 'use strict';
 
 module.exports.hello = async (event, context, callback) => {
+  const { first, second } = event;
+  const added = first + second;
   const response =  {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
+      event: added
     }),
   };
 
-  callback(new Error('something went wrong'));
+  callback(null, response);
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
