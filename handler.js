f781e0b96bdff56fe9bf353b3d875d2b6f1e9b6f
Hano <hinsul3113@gmail.com>
2019-05-12 00:03:57 -0700

    tested a url parameter, now our mini router is ready to deploy

--- handler.js
+++ handler.js
@@ -1,16 +1,30 @@
 'use strict';
 
 module.exports.hello = async (event, context, callback) => {
-  const { first, second } = event;
-  const added = first + second;
-  const response =  {
+
+  // const response =  {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: "Hello there!"
+  //   }),
+  // };
+
+  // callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+  // console.log(event.pathParameters);
+
+  const reply = `Hello ${event.pathParameters.name}`;
+
+  const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello there!"
-    }),
+      message: reply
+    })
   };
 
   callback(null, response);
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  
 };
